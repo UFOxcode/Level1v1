318099b8c4d0731fd7178066768eb6fb7d6a228e
ufoxcode <ufoxcode@gmail.com>
2019-12-26 22:04:03 +0800

    Initial Commit

---
+++ Level1v1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Level1v1
+//
+//  Created by UFO Xcode on 2019/12/26.
+//  Copyright © 2019 UFO Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Level1v1Tests/Level1v1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Level1v1Tests.swift
+//  Level1v1Tests
+//
+//  Created by UFO Xcode on 2019/12/26.
+//  Copyright © 2019 UFO Xcode. All rights reserved.
+//
+
+import XCTest
+@testable import Level1v1
+
+class Level1v1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
